8f7533919f32f1bd64afc604961bb4c085dd43b1
David M. Lyle <dlyle65535@gmail.com>
2014-03-26 13:05:19 +0000

    Testing overriden top level facts.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,10 +8,11 @@ describe "hadoop::init" do
      let :facts do
       {
          :osfamily => 'RedHat',
-         :operatingsystem => 'CentOS'
+         :operatingsystem => 'CentOS',        
       }
     end
-       
+	       	
+	
     it { should contain_class('hadoop::params') }
     #it { should contain_class('repos') }
     
@@ -107,6 +108,60 @@ describe "hadoop::init" do
     
 	        
   	end #context
+  	
+  	context 'Override All Default Values' do
+
+	     let :facts do
+	      {
+					:osfamily => 'RedHat',
+					:operatingsystem => 'CentOS',
+					:hadoop_node_version => '1.1.1',
+					:hadoop_node_master => 'master',
+					:hadoop_node_hdfsport => '1111',
+					:hadoop_node_replication => '10',
+					:hadoop_node_jobtrackerport => '1337',
+					 :hadoop_node_hadoop_base => '/base',
+					:hadoop_node_java_home =>'/java/home',				
+					:hadoop_file_buffer_size => '20',
+					:hadoop_tmp_dir => '/tmp/dir',
+					:hadoop_enable_private_actions => 'noWayMan',
+					:hadoop_compression_codecs => 'a,b,c',
+					:hadoop_lzo_class => 'lzoClass',
+					:hadoop_compress_map_output => 'Okay',
+					:hadoop_topology_script_file_name => 'topologyfile',
+					:hadoop_oozie_hosts => 'ooziehosts.com',
+					:hadoop_oozie_groups => 'ooziegroups',
+					:hadoop_node_job_tracker_http_server => 'my.jobtracker.com',
+					:hadoop_node_job_tracker_http_port => '80',
+					:hadoop_env_heapsize => '65535',
+					:hadoop_env_jobtracker_opts => 'javaserver',
+					:hadoop_env_namenode_opts => 'bigjava',
+					:hadoop_env_secondarynamenode_opts => 'medjava',
+	      }
+	    end
+	          			
+			it do      
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>fs.default.name<\/name>\n\t<value>hdfs:\/\/master:1111/)		
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>io.file.buffer.size<\/name>\n\t<value>20/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>hadoop.tmp.dir<\/name>\n\t<value>\/tmp\/dir/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>webinterface.private.actions<\/name>\n\t<value>noWayMan/)		
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>io.compression.codecs<\/name>\n\t<value>a,b,c/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>io.compression.codec.lzo.class<\/name>\n\t<value>lzoClass/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>mapred.compress.map.output<\/name>\n\t<value>Okay/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>topology.script.file.name<\/name>\n\t<value>topologyfile<\/value>/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>hadoop.proxyuser.oozie.hosts<\/name>\n\t<value>\ooziehosts.com/)
+				should contain_file('/base/conf/core-site.xml').with_content(/<name>hadoop.proxyuser.oozie.groups<\/name>\n\t<value>\ooziegroups/)
+	      should contain_file('/base/conf/hdfs-site.xml').with_content(/<name>dfs.replication<\/name>\n\t<value>10/)			
+	    	should contain_file('/base/conf/mapred-site.xml').with_content(/<name>mapred.job.tracker<\/name>\n\t<value>master:1337/)		
+				should contain_file('/base/conf/mapred-site.xml').with_content(/<name>mapreduce.jobtracker.http.address<\/name>\n\t<value>my\.jobtracker\.com:80/)
+				should contain_file('/base/conf/hadoop-env.sh').with_content(/export JAVA_HOME=\/java\/home/)
+				should contain_file('/base/conf/hadoop-env.sh').with_content(/export HADOOP_HEAPSIZE=65535/)
+				should contain_file('/base/conf/hadoop-env.sh').with_content(/export HADOOP_JOBTRACKER_OPTS=javaserver/)
+				should contain_file('/base/conf/hadoop-env.sh').with_content(/export HADOOP_NAMENODE_OPTS=bigjava/)
+				should contain_file('/base/conf/hadoop-env.sh').with_content(/export HADOOP_SECONDARYNAMENODE_OPTS=medjava/)
+			end
+  		
+  	end
   end   
   		
 
